678b3e52826fdd058aedaa56be0a8fe05a440cc6
froberg1234 <48415993+froberg1234@users.noreply.github.com>
2022-03-14 19:34:54 +0100

    Tilføjet nyt kasino, tilføjet håndholdt fartmåler, Tilføjet nossler til tankstaionen tilføjet neon menu på F9
    Tilføjet nyt kasino, tilføjet håndholdt fartmåler, Tilføjet nossler til tankstaionen tilføjet neon menu på F9

---
+++ resources/[standalone]/RadarGun/config.lua
@@ -0,0 +1,15 @@
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+--	   keybinds = https://docs.fivem.net/game-references/controls/	  --
+--     							 									  --
+--	   For support join my discord: https://discord.gg/Z9Mxu72zZ6	  --
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+
+config = {
+	radarGun = "WEAPON_VINTAGEPISTOL", -- Weapon you want to use as the radargun default is "WEAPON_VINTAGEPISTOL".
+	metric = false, -- true will use the metric system and false will use the imperial system
+	enableALPR = true, -- true will enable the plate reader on the radar gun.
+	enableCrosshair = true, -- enable or disable the red dot cross hair in 3rd person.
+	defaultRange = 110.0, -- def ault range the radar gun can reach in ft.
+}
---
+++ resources/[standalone]/RadarGun/fxmanifest.lua
@@ -0,0 +1,30 @@
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+--			DO NOT EDIT IF YOU DON'T KNOW WHAT YOU'RE DOING			  --
+--     							 									  --
+--	   For support join my discord: https://discord.gg/Z9Mxu72zZ6	  --
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+
+fx_version "cerulean"
+game "gta5"
+
+author "Andyyy#7666"
+description "RadarGun"
+version "1.2"
+
+ui_page "html/index.html"
+
+files {
+	"html/index.html",
+	"html/js/jquery-3.6.0.min.js",
+	"html/img/radargun.png",
+	"html/img/scope.png",
+	"html/js/listener.js",
+	"html/style.css",
+}
+
+client_scripts {
+	"config.lua",
+	"client.lua",
+}
