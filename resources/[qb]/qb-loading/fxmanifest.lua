9ae7d73a8ea8ac437b4e2cdd38c7663986b3634d
froberg1234 <48415993+froberg1234@users.noreply.github.com>
2022-03-16 01:02:03 +0100

    Ny og forbedret loadingscreen
    Ny og forbedret loadingscreen

--- resources/[qb]/qb-loading/fxmanifest.lua
+++ resources/[qb]/qb-loading/fxmanifest.lua
@@ -1,14 +1,22 @@
 fx_version 'cerulean'
-game 'gta5'
 
-description 'qb-loading'
-version '1.0'
+game {'gta5'}
 
-lua54 'yes'
-client_script 'client/client.lua'
+description 'ns-loadingscreen'
+version '1.0.0'
+author 'github.com/IN1GHTM4R3'
 
-files { 'assets/**', 'html/*', }
+--client_script {'client.lua'}
 
-loadscreen { 'html/index.html' }
+files {
+    'ui.html',
+    'html/*',
+    'assets/*',
+}
+
+loadscreen {
+    'ui.html',
+}
+
 loadscreen_cursor 'yes'
-loadscreen_manual_shutdown 'yes'
+--loadscreen_manual_shutdown 'yes'
