acf812de1f79d2bcfb92eef0795ac52062e23809
froberg1234 <48415993+froberg1234@users.noreply.github.com>
2022-03-16 01:05:36 +0100

    Tilføjet K9 script
    Tilføjet K9 script

--- resources/[qb]/qb-inventory/server/main.lua
+++ resources/[qb]/qb-inventory/server/main.lua
@@ -33,6 +33,64 @@ local function hasCraftItems(source, CostItems, amount)
 	return true
 end
 
+local function HasItem(list, item)
+
+	for i = 1, #list do
+
+		if item == list[i] then
+			return true
+		end
+	end
+
+	return false
+end
+
+AddEventHandler("inventory:server:SearchLocalVehicleInventory", function(plate, list, cb)
+local trunk = Trunks[plate]
+local glovebox = Gloveboxes[plate]
+local result = false
+
+if trunk ~= nil then
+	for k, v in pairs(trunk.items) do
+		local ITEM = trunk.items[k].name
+		if HasItem(list, ITEM) then
+			RESULT = true
+		end
+	end
+else
+	trunk = GetOwnedVehicleItems(plate)
+
+	for k, v in pairs(TRUNK) do
+
+		local ITEM = TRUNK[k].name
+		if HasItem(list, ITEM) then
+			RESULT = true
+		end
+	end
+
+end
+
+if glovebox ~= nil then
+	for k, v in pairs(glovebox.items) do
+
+		local ITEM = glovebox.items[k].name
+		if HasItem(list, ITEM) then
+			RESULT = true
+		end
+	end
+else
+	glovebox = GetOwnedVehicleGloveboxItems(plate)
+
+	for k, v in pairs(glovebox) do
+		local ITEM = glovebox[k].name
+		if HasItem(list, ITEM) then
+			RESULT = true
+		end
+	end
+end
+cb(RESULT)
+end)
+
 local function IsVehicleOwned(plate)
     local result = MySQL.Sync.fetchScalar('SELECT 1 from player_vehicles WHERE plate = ?', {plate})
     if result then return true else return false end
