13f7c7bc89acd8530c70b1b61c582cf797809f3a
froberg1234 <47919859+7R3C3@users.noreply.github.com>
2022-03-21 17:05:52 +0100

    Nyt hospital [under udvikling]
    Nyt hospital [under udvikling]

---
+++ resources/[defaultmaps]/fivem_gn_mountzonah/fxmanifest.lua
@@ -0,0 +1,64 @@
+fx_version "cerulean"
+games { 'gta5' }
+
+author 'GN_Mods'
+description 'Mount_Zonah'
+version '1.3.3'
+
+dependencies {
+    '/gameBuild:2545'
+}
+this_is_a_map 'yes'
+
+--[[
+Update 1.3.3
+- Fix of mz_addons.ytyp
+- Added different gamebuild compatibility ymaps
+- Added different compatibility ymap if you use a cityhall in front of the mounzonah
+
+Update 1.3.2
+
+- Fixed the problems of the DLC "THE CONTRAT"
+- Fixed the dispenser/glass bug
+- Improved external occlusion
+- Modification of some misplaced props
+- Resource optimization
+
+Update 1.3.1
+
+- Resource optimization 90,2 mo -> 46.4mo(AltV) / 73.5mo(FiveM)
+- Rooftop collision enhancement
+- Rest room props fixed
+- Corridor (room floor) light improvement
+- Pharmacy light improvement
+- Articulated arm light improvement (operating room)
+
+Addition Update 1.3
+
+- Improvement and new furnishing of the pharmarcy
+- New entrance floor on helipad
+- Light optimization in the main entrance
+- NVE mod compatible version
+- Static bed and more in rest room and corridor (emergency)
+- Opening the lower office doors
+- Smoothing pillars and more of building
+
+Update 1.2
+
+- Mink problem with the outside on the intermediate floor
+- Fixed exterior collision
+- Vision problem upper floor offices fixed
+- LOD increase for the carpack
+
+Update 1.1
+
+- Collision upgrade (different floor & exterior)
+- "pharamacy" texture correction 
+- Fixed texture of  stair elevator panel
+- Pharmacy addition wall decoration
+- External portals optimization in emergency part
+- Heliport  mapping addition on rooftop
+- Add teleportation script heliport
+- Sliding door texture modification
+]]
+
